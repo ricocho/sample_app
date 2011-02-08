86c8c4442e8830b2ab9faf204e922e5db943edcd
Enrique Garmendia <ejgarm@att.net>
2011-02-08 00:02:01 -0600

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,6 +13,13 @@ describe PagesController do
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
+      response.should be_success
+    end
+  end
+  
+    describe "GET 'contact'" do
+    it "should be successful" do
+      get 'about'
       response.should be_success
     end
   end
