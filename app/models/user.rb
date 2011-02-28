bf9d153c5f84e3df18197607ed01fa4ee82e06e7
Enrique Garmendia <ejgarm@att.net>
2011-02-27 20:36:15 -0600

    Transfer

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,15 +11,43 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
   
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name, :presence => true,
-            :length => { :maximum => 50 },
-            :uniqueness => true
+                    :length => { :maximum => 50 },
+                    :uniqueness => true
   validates :email, :presence => true,
-            :format => { :with => email_regex }
-
+                    :format => { :with => email_regex }
+  validates :password, :presence => true,
+                      :length => { :within => 6..40 },
+                      :confirmation => true
+            
+  before_save :encrypt_password
+  
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  private
+  
+    def encrypt_password 
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)  
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,11 @@ require 'spec_helper'
 describe User do
   
   before(:each) do
-    @attr = { :name => "Example User" , :email => "user@example.com" }
-  end
+    @attr = { :name => "Example User" ,
+              :email => "user@example.com",
+              :password => "foobar",
+              :password_confirmation => "foobar" }
+   end
   
   it "should create a new instance given valid attibutes" do
     User.create!(@attr)
@@ -13,6 +16,53 @@ describe User do
   it "should require name" do
     no_name_user = User.new(@attr.merge(:name => ""))
     no_name_user.should_not be_valid
+  end
+  
+  
+  describe "password validations" do
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).should_not be_valid
+    end
+    
+    it "should require a passing confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).should_not be_valid
+    end
+    
+    it "should reject a short password" do
+      short = "a" * 5
+      User.new(@attr.merge(:password => "short", :password_confirmation => "short")).should_not be_valid
+    end
+    
+    it "should reject a long password" do
+      long = "a" * 50
+      User.new(@attr.merge(:password => "long", :password_confirmation => "long")).should_not be_valid
+    end
+  end 
+  
+  describe "password encryption" do
+    before(:each) do
+      @user = User.create(@attr)
+    end
+    
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+    
+    it "should set an encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end
+    
+    describe "has_password? method" do
+      
+      it "should be true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+      
+      it "should be false if the passwords don't match" do
+        @user.has_password?("invalid").should be_false
+      end
+    end
+    
   end
   
 end
