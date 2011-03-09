6d7fd6edaf770aa9b67a14612cd449cdd1942f7c
ricocho <ejgarm@att.net>
2011-03-08 21:29:28 -0600

    Done with sign in

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -40,5 +40,31 @@ describe "LayoutLinks" do
      click_link "Sign Up now!"
      response.should have_selector('title', :content => "Sign Up")     
    end
-    
+   
+   describe 'when not sign in' do
+     it 'shouldhave a signin links' do
+       visit root_path
+       response.should have_selector("a", :href => signin_path, :content => "Sign in")
+     end
+   end
+   
+   describe 'when signed in' do
+     before(:each) do
+       @user = Factory(:user)
+       visit signin_path
+       fill_in :email,    :with => @user.email
+       fill_in :password, :with => @user.password
+       click_button   
+     end
+   
+   it 'should have a signout link' do
+     visit root_path
+     response.should have_selector("a", :href => signout_path, :content => "Sign out")
+   end
+   
+   it 'should have profile links' do
+     visit root_path
+     response.should have_selector("a", :href => user_path(@user), :content => "Profile")
+   end 
+  end
 end
